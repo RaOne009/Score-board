8018c33b9f57c5a57454118d973d6c9ef4b72d0b
RaOne009 <rs1257933@gmail.com>
2019-10-11 00:42:16 +0530

    Modified

--- score.js
+++ score.js
@@ -1,13 +1,57 @@
-window.onload=function(){
-
+/*
+Rohit Sharma
+GitHub :- RaOne009
+*/
 var b1 = document.querySelector("#p1");
-var b2 = document.getElementById("p1");
-// b2.addEventListner("click", function(){
-//     alert("clicked");
-// });
-for (var i = 0; i < b2.length; i++) {
-  b2[i].addEventListener('click', function(){
-      alert("clicked");
-  });
-}
+var b2 = document.getElementById("p2");
+var b3 = document.getElementById("reset");
+var numinput = document.querySelector("input[type = 'Number']")
+var display1 = document.getElementById("display1");
+var display2 = document.getElementById("display2");
+var winningscoredisplay = document.querySelector("p span");
+document.body.style.background = "#4ff0de24";
+var p1 = 0;
+var p2 = 0;
+var gameover = false;
+var winingscore = 5;
+b1.addEventListner("Click", function(){
+    if (!gameover) {
+        p1++;
+        if (p1 === winingscore) {
+            display1.classList.add("winner");
+            display2.classList.add("loser");
+            gameover = true;
+        }
+        display1.textcontent = p1;
+    }
+});
+b2.addEventListner("Click", function(){
+    if (!gameover) {
+        p2++;
+        if (p2 === winingscore) {
+            display2.classList.add("winner");
+            display1.classList.add("loser");
+            gameover == true;
+        }
+        display2.textcontent = p2;
+    }
+});
+b3.addEventListner("Click", function(){
+reset();
+});
+numinput.addEventListner("Click", function(){
+    winningscoredisplay.textcontent = this.value;
+    winingscore = Number(this.value);
+    reset();
+});
+function reset() {
+    p1 = 0;
+    p2 = 0;
+    gameover = false;
+    display1.textcontent = 0;
+    display2.textcontent = 0;
+    display1.classList.remove("winner");
+    display1.classList.remove("winner");
+    display2.classList.remove("loser");
+    display2.classList.remove("loser");
 }
